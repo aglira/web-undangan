7dc4ce962f1aeb6d590400bf812de71378fc8a8f
aglira <agniramadhani5@gmail.com>
2023-03-14 20:47:58 +0700

    update project

--- present.js
+++ present.js
@@ -4,11 +4,11 @@ form.addEventListener("submit",e=>{
      e.preventDefault()
      const formData = Object.fromEntries(new FormData(e.target))
      if (formData.kehadiran  == 'tidak_hadir') {
-     handleRedirect('web-undangan/notpresent.html')
+     handleRedirect('/notpresent.html')
      //    console.log('a');
      }
      else{
-     handleRedirect('web-undangan/present.html')
+     handleRedirect('/present.html')
      }
      console.log(formData.kehadiran)
 })
